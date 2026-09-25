fa6a332dfe8716cad1b3c3b4fda8ac88dcd5c47f
Sakthi Preethika K <Sakthipreethika315@gmail.com>
2026-09-25 09:40:59 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,50 @@
+class PrefixTree {
+    class Node{
+        HashMap<Character,Node> map=new HashMap<>();
+        boolean end=false;
+    }
+    Node root;
+
+    public PrefixTree() {
+        root=new Node();
+
+         
+    }
+
+    public void insert(String word) {
+        Node curr=root;
+        for(char c:word.toCharArray()){
+            if(!curr.map.containsKey(c)){
+                curr.map.put(c,new Node());
+            }
+            curr=curr.map.get(c);
+        }
+        curr.end=true;
+
+    }
+
+    public boolean search(String word) {
+        Node curr=root;
+        for(char c:word.toCharArray()){
+            if(!curr.map.containsKey(c)){
+                return false;
+            }
+            curr=curr.map.get(c);
+
+        }
+        return curr.end;
+
+    }
+
+    public boolean startsWith(String prefix) {
+        Node curr=root;
+        for(char c:prefix.toCharArray()){
+            if(!curr.map.containsKey(c)){
+                return false;
+            }
+            curr=curr.map.get(c);
+        }
+        return true;
+
+    }
+}
